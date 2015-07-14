a740642a795adfa0a2c59734d30c4f1f08de20e9
DaveVoyles <Startingline13@hotmail.com>
2015-07-14 09:56:58 -0400

    Changed web config for mp4

--- js/main.js
+++ js/main.js
@@ -41,9 +41,11 @@
 
     var initializeVideoStream = function (stream) {
         video = videojs('video-player');
-        video.src({ type: 'video/mp4', src: introVid });
+        video.src({ type: 'video/mp4', src: "https://medianighttrap.blob.core.windows.net/asset-9412435d-1500-80c3-6f56-f1e529d32336/Bedroom.mp4?sv=2012-02-12&sr=c&si=a7d1fbe9-58bd-45e2-8d2d-09e04d2e817d&sig=xYng3DacbYamyksaGGRtHYo6ul8dGFuCxyioxwQ%2FGzA%3D&st=2015-07-14T03%3A02%3A16Z&se=2115-06-20T03%3A02%3A16Z" });
         video.load();
     };
+
+   
 
 
     // changeVideoStream() - Returns a new video stream
