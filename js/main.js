80d7e66689287e51844bd9ef7f279627cf2a6e61
Dave Voyles <dnvoyles@gmail.com>
2015-09-27 16:43:29 -0400

    more refactoring

--- js/main.js
+++ js/main.js
@@ -273,14 +273,18 @@ var mainJS = (function() {
     /**
      * How many augers has the user missed?
      */
-    var nTotalMissed           = {
-        missed: 0,
-        get: function () {
-            return this.missed;
-        },
-        increment: function (val) {
-            this.missed        += val;
-        }
+    var nTotalMissed = function nTotalMissed () {
+        this._missed = 0;
+        
+        var nTotalMissedPrototype = {
+            get: function () {
+                return this._missed;
+            },
+            increment: function (val) {
+                this._missed += val;
+            }
+        };
+        return nTotalMissedPrototype;
     };
 
     // Timer to keep track of user's time spent in-game
@@ -333,31 +337,31 @@ var mainJS = (function() {
         generateRanPass:  function () {
             var result;
             var count = 0;
-          for (var prop in this.passwords)
-              if (Math.random() < 1/++count) {
-                result = prop;
-                switch(result) {
-                  case Purple:
-                    playSfx(sfxPath.Purple);
-                    break;
-                  case Blue:
-                    playSfx(sfxPath.Blue  );
-                    break;
-                  case Red:
-                    playSfx(sfxPath.Red   );
-                    break;
-                  case Green:
-                    playSfx(sfxPath.Green );
-                    break;
-                  case Yellow:
-                    playSfx(sfxPath.Yellow);
-                    break;
-                  case Orange:
-                    playSfx(sfxPath.Orange);
-                    break;
+            for (var prop in this.passwords)
+                if (Math.random() < 1/++count) {
+                    result = prop;
+                    switch(result) {
+                    case Purple:
+                        playSfx(sfxPath.Purple);
+                        break;
+                    case Blue:
+                        playSfx(sfxPath.Blue  );
+                        break;
+                    case Red:
+                        playSfx(sfxPath.Red   );
+                        break;
+                    case Green:
+                        playSfx(sfxPath.Green );
+                        break;
+                    case Yellow:
+                        playSfx(sfxPath.Yellow);
+                        break;
+                    case Orange:
+                        playSfx(sfxPath.Orange);
+                        break;
+                    }
                 }
-              }
-            return result;
+                return result;
         },
         setStyleColor: function () {
           switch(this.pass){
@@ -399,111 +403,6 @@ var mainJS = (function() {
         'video/00352291.mp4',
         'video/00431292.mp4'
     ];
-
-    /* 0 | 1 Opening & General */
-    var camMisc                = {
-        DPWORLDD: '',
-        // Intro briefing
-        c11: 'https://nighttrap.blob.core.windows.net/vid/intro/00000011-Intro.mp4'
-    };
-
-    /* 2 - Hall-1 */
-    var camHallOne             = {
-         // Augers enter through back door, walk to basement
-          c21:      'https://nighttrapblob.blob.core.windows.net/ntvids/hallOne/00000021.mp4'
-        // TRAP: Augers captured in hall
-        , c130422:  'https://nighttrapblob.blob.core.windows.net/ntvids/hallOne/00130422.mp4'
-        // Tony, Jeff, & Dad enter from basement
-        , c1152221: 'https://nighttrapblob.blob.core.windows.net/ntvids/hallOne/02500221.mp4'           //TODO: AZURE: Labeled wrong
-    };
-
-    /* 3 - Kitchen */
-    var camKitchen             = {
-         // 1 Auger walks in from Entry. Can catch at 4 Sec
-          c1200431: 'https://nighttrapblob.blob.core.windows.net/ntvids/kitchen/01200431.mp4'
-        // 1 Auger captured in kitchen when trying to access fridge
-        , c1240632: 'https://nighttrapblob.blob.core.windows.net/ntvids/kitchen/01240632.mp4'
-        // Tony, Jeff, & Dad enter from Hall-1, talk to parents
-        , c1481231: 'https://nighttrapblob.blob.core.windows.net/ntvids/kitchen/01481231.mp4'
-    };
-
-    /* 4- Living-Room */
-    var camLivingRoom          = {
-        // Mom enters from bookshelf
-          c1572241: 'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/01572241.mp4'
-         // Augers enter from outside (branches)
-         ,c232241:  'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/00232241.mp4'
-        // TRAP: Augers captured on bookshelf
-        , c271442:  'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/00271442.mp4'
-        // Augers Escape  
-        , c271641:  'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/00271641.mp4'
-        // TRAP: Auger captured on library
-        , c554164a: 'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/0554164a.mp4'
-        // 2 Augers enter from outside
-        , c1001241 :'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/01001241.mp4'
-        // TRAP: Augers captured on right side of living room
-        , c1071042: 'https://nighttrapblob.blob.core.windows.net/ntvids/livingroom/01071042.mp4'
-    };
-
-    /* 5 - Driveway */
-    var camDriveway            = {
-        //  Girls enter the driveway, meet eddy, walk in. Can catch at ~6 Sec.
-          c1440451: 'https://nighttrapblob.blob.core.windows.net/ntvids/driveway/01440451.mp4'
-        // TRAP: Launching auger from roof
-        , c1502452: 'https://nighttrapblob.blob.core.windows.net/ntvids/driveway/01502452.mp4'
-    };
-
-    /* 6 - Entryway */
-    var camEntryway            = {
-        // 1 Auger walks in from beneath stairs. Looks outside. Can be captured
-          c1320261: 'https://nighttrapblob.blob.core.windows.net/ntvids/driveway/01502452.mp4'
-        // 1 Auger captured in entryway
-        , c1391862: 'https://nighttrapblob.blob.core.windows.net/ntvids/entryway/01391862.mp4'
-        // Sarah enters from closet, parents enter, augers can be trapped on stairs
-        , c2122461: 'https://nighttrapblob.blob.core.windows.net/ntvids/entryway/02122461.mp4'
-        // Tony & Jeff walk in from kitchen, argue about door, walk outside
-        , c2500221: 'https://medianighttrap.blob.core.windows.net/asset-e41e435d-1500-80c4-3ba5-f1e52dbb97ae/02500221.mp4?sv=2012-02-12&sr=c&si=f34e6bd8-dbe4-464f-b0f8-2b16c61fcecd&sig=DretusvijWM7WVsXbipYK6W%2FBjEHDn9jXwsxg8%2F3zyE%3D&st=2015-07-19T02%3A18%3A59Z&se=2115-06-25T02%3A18%3A59Z'
-    };
-
-    /* 7 - Hall-2 */
-    var camHallTwo             = {
-        // Auger enters hall 2 from bedroom
-          c310471: 'https://nighttrapblob.blob.core.windows.net/ntvids/hallTwo/00310471.mp4'
-        // Auger walks in from Bathroom.
-        , c500271: 'https://nighttrapblob.blob.core.windows.net/ntvids/hallTwo/00500271.mp4'
-        // Auger captured in hall trap
-        , c542272: 'https://nighttrapblob.blob.core.windows.net/ntvids/hallTwo/00542272.mp4'
-        // Augers enters from bedroom, goes down stairs
-        , c2390671: 'https://nighttrapblob.blob.core.windows.net/ntvids/hallTwo/02390671.mp4'
-    };
-
-    /* 8 - Bedroom */
-    var camBedroom             = {
-        //Sarah staring at mirror, 3 augers enter two go to bathroom one to hall-2
-           c81:    'https://nighttrapblob.blob.core.windows.net/ntvids/bedroom/00000081.mp4'
-        // TRAP: Augers captured
-        , c130422: 'https://medianighttrap.blob.core.windows.net/asset-e41e435d-1500-80c4-a05f-f1e52dbb857e/00130422.mp4?sv=2012-02-12&sr=c&si=681815ff-ed6f-4acf-861c-3886316945ee&sig=vUMKUifi5f3Pcj7WlVQqy4R0FYJ6AF9%2BjPQXbdMaONc%3D&st=2015-07-19T02%3A18%3A44Z&se=2115-06-25T02%3A18%3A44Z'
-        // TRAP: 1 Auger is captured, other walks into bathroom
-        , c352482: 'https://nighttrapblob.blob.core.windows.net/ntvids/bedroom/00352482.mp4'
-        // Auger walks in from Bathroom, goes out window
-        , c540281: 'https://nighttrapblob.blob.core.windows.net/ntvids/bedroom/00540281.mp4'
-    };
-
-    /* 9 - Bathroom */
-    var camBathroom            = {
-        // Sarah enters bathroom from bedroom. Enters mirror
-          c180291:   'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/00180291.mp4'
-        // 2 Augers enter bathroom from bedroom, 1 catchable
-        , c352291:   'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/00352291.mp4'
-        // TRAP: 1 Auger is captured, other walks into mirror
-        , c430249b:  'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/0430249b.mp4'
-        // TRAP: Auger captured on scale trap
-        , c431292:   'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/00431292.mp4'
-        // Auger walks in from Hall-2, walks toward bedroom
-        , c480291:   'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/00480291.mp4'
-        // TRAP: Auger captured in floor trap
-        , c500291:   'https://nighttrapblob.blob.core.windows.net/ntvids/bathroom/00500291.mp4'
-    };
 
     /**
      * Wires up event handlers for buttons.
