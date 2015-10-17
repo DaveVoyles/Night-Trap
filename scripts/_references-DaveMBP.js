e79a6d4a43d2e03b412bdfce916fc1c80b487292
Dave Voyles <dnvoyles@gmail.com>
2015-10-17 12:21:54 -0400

    going to force

---
+++ scripts/_references-DaveMBP.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../js/cameras.js" />
+/// <reference path="../js/main.js" />
+/// <reference path="../js/rooms.js" />
+/// <reference path="../js/vendor/mainloop/mainloop.js" />
+/// <reference path="../js/vendor/moment/moment.js" />
+/// <reference path="../bower_components/video.js/dist/video.js"/>
